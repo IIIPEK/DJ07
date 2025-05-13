6fdf694d9318e79ef39e1f1d39b5484e3ef24407
Ivan Vorobev <ivan.vorobjov@gmail.com>
2025-05-13 16:52:38 +0300

    POST tested

--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from bot.models import TelegramUser
+from bot.serializers import TelegramUserSerializer
+
+
+@api_view(['POST'])
+def register_user(request):
+    data = request.data
+    user, created = TelegramUser.objects.get_or_create(
+        user_id=data['user_id'],
+        defaults={
+            'username': data.get('username',''),
+        }
+    )
+    serializer = TelegramUserSerializer(user)
+    if created:
+        serializer = TelegramUserSerializer(user)
+        return Response(serializer.data, status=201)
+    else:
+        return Response({'message': 'User already exists'})
